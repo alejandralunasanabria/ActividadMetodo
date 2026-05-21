f71fbdd61a082a6c262eabcb724178d7a26f8fcb
Luna Alejandra Sanabria Aponte <lalejandrasanabria@ucompensar.edu.co>
2026-05-20 21:10:02 -0500

    Agrega clase mostrarMensaje en rama luna

---
+++ mostrarMensaje.java
@@ -0,0 +1,9 @@
+public class mostrarMensaje {
+    public static void mostrarMensaje(String mensaje){
+        System.out.println("mensaje: " + mensaje);
+    }
+
+    public static void main(String[] args) {
+        mensaje("Mostrar Mensaje");
+    }
+}
