cdf7bf151492736a6fa4150e2a3066d94fa17a7c
David Alejandro Martinez Pinzon <davidalejandromartinez@ucompensar.edu.co>
2026-05-20 21:14:16 -0500

    Realice el metodo para mostrar los dias de la semana

--- ActividadesMetodos.java
+++ ActividadesMetodos.java
@@ -1,13 +1,10 @@
 public class ActividadesMetodos {
-
-    // Método que calcula y retorna la suma de dos números enteros
-    public static int sumar(int num1, int num2) {
-        int resultado = num1 + num2;
-        return resultado;
+    public static void main(String[] args) {
+        mostrarDiaSemana();
     }
 
-    public static void main(String[] args) {
-        // Llamada al método sumar e impresión del resultado en consola
-        System.out.println(sumar(7, 8)); 
+    // Aquí agregamos 'static' antes de 'void'
+    public static void mostrarDiaSemana(){
+        System.out.println("Lunes Martes Miercoles Jueves Viernes Sabado Domingo");
     }
 }
