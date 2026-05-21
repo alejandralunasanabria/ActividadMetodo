70e452d2711a626eaa8ca942a9b2f5282d2118ca
Luna Alejandra Sanabria Aponte <lalejandrasanabria@ucompensar.edu.co>
2026-05-20 21:16:32 -0500

    Cambios en archivo desde luna

--- mostrarMensaje.java
+++ mostrarMensaje.java
@@ -1,8 +1,12 @@
 public class mostrarMensaje {
+
+    //Crear metodo
     public static void mostrarMensaje(String mensaje){
         System.out.println("mensaje: " + mensaje);
     }
 
+
+    //llamar metodo
     public static void main(String[] args) {
         mensaje("Mostrar Mensaje");
     }
