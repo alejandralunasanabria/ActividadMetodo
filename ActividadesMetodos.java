b1676ce4dee39faec1533b33c6b2d166322ba164
David Alejandro Martinez Pinzon <davidalejandromartinez@ucompensar.edu.co>
2026-05-20 20:59:01 -0500

    prueba

---
+++ ActividadesMetodos.java
@@ -0,0 +1,13 @@
+public class ActividadesMetodos {
+
+    // Método que calcula y retorna la suma de dos números enteros
+    public static int sumar(int num1, int num2) {
+        int resultado = num1 + num2;
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        // Llamada al método sumar e impresión del resultado en consola
+        System.out.println(sumar(7, 8)); 
+    }
+}
